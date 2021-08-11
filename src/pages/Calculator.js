a34fb1460c0f300818c36fb439e5a22185b291d6
Silviu <silviu074@gmail.com.com>
2021-08-11 22:41:57 +0300

    retus final

--- src/pages/Calculator.js
+++ src/pages/Calculator.js
@@ -1,14 +1,14 @@
 export const calculator = {
   flightDistance: (n1) => {
-    return n1 * 1.35;
+    return Math.round(n1 * 0.55);
   },
   flightHousing: (n1, n2) => {
-    return n1 + n2;
+    return Math.round(n1 + n2);
   },
   tva: (n1) => {
-    return (n1 * 19) / 100;
+    return Math.round((n1 * 19) / 100);
   },
   guestDiscount: (n1, n2) => {
-    return n1 - n2 * 3.3;
+    return Math.round(n1 - n2 * 3.3);
   },
 };
